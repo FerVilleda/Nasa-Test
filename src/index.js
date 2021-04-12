6408459f5fdd6c7c8aac4a681143eae7db119578
FerVilleda <fer_mcr_95@hotmail.com>
2021-04-12 13:32:24 -0500

    eliminar provider sin usar

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App.jsx';
 
 import FotosProvider from './context/FotosProvider'
-import URLProvider from './context/URLProvider';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <URLProvider>
+   
       <FotosProvider>
         <App />
       </FotosProvider>
-    </URLProvider> 
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
